ed6aac2d583ed3d9f79d48ad459bce2f1ba88112
alari <name.alari@gmail.com>
2012-08-01 11:57:54 +0400

    extension is made

---
+++ I18n2ascii.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @author alari
+ * @since 7/23/12 3:07 PM
+ *
+ * Usage:
+ * <code>
+ * public function beforeSave()
+ * {
+ *  if(Yii::app()->getComponent("i18n2ascii")) {
+ *      Yii::app()->getComponent("i18n2ascii")->setModelUrlAlias($this, $this->title);
+ *  }
+ *  return true;
+ * }
+ * </code>
+ *
+ * <code>
+ * 'i18n2ascii' => array(
+ *      'class' => 'application.extensions.i18n2ascii.I18n2ascii'
+ * )
+ * </code>
+ */
+class I18n2ascii extends CComponent
+{
+    private $map = Array();
+    private $mapsDir = "protected/messages/";
+
+    public function init() {}
+
+    public function setModelUrlAlias(CActiveRecord $model, $path, $attr="path", $stringSuffix = "p") {
+        if (!$model->$attr) {
+            $model->$attr = $this->convertToUrl($path);
+
+            if(is_numeric($model->$attr)) $this->$attr .= $stringSuffix;
+
+            while ($model->countByAttributes(array($attr => $model->$attr))) $model->$attr .= rand(0, 9);
+        }
+    }
+
+    public function convertToUrl($string)
+    {
+        return preg_replace('/[^A-Za-z0-9_\-]/', '', $this->convertToAscii($string));
+    }
+
+    public function convertToAscii($string)
+    {
+        if (!count($this->map)) {
+            self::loadMap();
+        }
+        if (!count($this->map)) {
+            return $string;
+        }
+        return strtr(trim($string), $this->map);
+    }
+
+    private function loadMap()
+    {
+        foreach (glob(dirname(__FILE__) . "/*.txt") as $fn) {
+            $this->map = array_merge($this->map, parse_ini_file($fn));
+        }
+        foreach (glob($this->mapsDir . "*.txt") as $fn) {
+            $this->map = array_merge($this->map, parse_ini_file($fn));
+        }
+        $this->map[" "] = "-";
+
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 yii-i18n2ascii
 ==============
 
-Yii extension to handle transliteration and some kind of internationalization
+Yii extension to handle transliteration and some kind of internationalization.
+
+=Usage=
+
+Add submodule:
+
+`git add submodule git://github.com/alari/yii-i18n2ascii.git protected/extensions/i18n2ascii`
+
+Register the component:
+
+```
+'i18n2ascii' => array(
+       'class' => 'application.extensions.i18n2ascii.I18n2ascii'
+)
+```
+
+Use it in your modeles or directly:
+
+```
+public function beforeSave()
+  {
+   if(Yii::app()->getComponent("i18n2ascii")) {
+       Yii::app()->getComponent("i18n2ascii")->setModelUrlAlias($this, $this->title);
+   }
+   return true;
+  }
+```
+
+You can use Drupal i18n2ascii files to extend transliteration rules. Place additional files into your `messages` folder.
